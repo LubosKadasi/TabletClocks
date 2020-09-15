82af031a86788a30c6aa6b6cc39b6d9069ac20fb
NENO\kadoo <kadoox@gmail.com>
2020-09-15 22:07:20 +0200

    v12

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -222,7 +222,7 @@ bgImage.onreadystatechange = function() {
 };
 
 function getBgImage(){
-    bgImage.open("GET", "//source.unsplash.com/random/1280x720", true);
+    bgImage.open("GET", "//source.unsplash.com/random/1280x720/?nature,architecture,space,featured", true);
     bgImage.send();
 };
 
@@ -232,21 +232,10 @@ function getBgImage(){
     https://googlechrome.github.io/samples/battery-status/
 */
 
-var chargingStateEl = document.getElementById('chargingState');
-var chargingTimeEl = document.getElementById('chargingTime');
-var dichargeTimeEl = document.getElementById('dischargeTime');
 var levelEl = document.getElementById('level');
 
 function updateBatteryUI(battery) {
     levelEl.textContent = (battery.level * 100) + '%';
-    chargingTimeEl.textContent = battery.chargingTime + ' Seconds to full charge';
-    dichargeTimeEl.textContent = battery.dischargingTime + ' Seconds to empty';
-
-    if (battery.charging === true) {
-        chargingStateEl.textContent = 'Charging';
-    } else if (battery.charging === false) {
-        chargingStateEl.textContent = 'Discharging';
-    }
 };
 
 function monitorBattery(battery) {
@@ -256,18 +245,12 @@ function monitorBattery(battery) {
     // Monitor for futher updates.
     battery.addEventListener('levelchange',
         updateBatteryUI.bind(null, battery));
-    battery.addEventListener('chargingchange',
-        updateBatteryUI.bind(null, battery));
-    battery.addEventListener('dischargingtimechange',
-        updateBatteryUI.bind(null, battery));
-    battery.addEventListener('chargingtimechange',
-        updateBatteryUI.bind(null, battery));
 };
 
 if ('getBattery' in navigator) {
     navigator.getBattery().then(monitorBattery);
 } else {
-    ChromeSamples.setStatus('The Battery Status API is not supported on ' +
+    console.log('The Battery Status API is not supported on ' +
         'this platform.');
 };
 
